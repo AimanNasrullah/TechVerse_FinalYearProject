d9e58a3dd3ffdf25fde015e636dd57f2a60e2409
Aiman Nasrullah <a.nasrullahzullkifli@gmail.com>
2024-09-10 12:38:55 +0800

    Added db config

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__) # flask initialization
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/fypDB'
-    # db.init_app(app)
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/{}'
+    db.init_app(app)
+    
     return app
